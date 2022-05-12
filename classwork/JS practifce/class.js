5203fcf38db3fe6d37e3822f9f90c29949a24b6b
Kdenn1 <103376676+Kdenn1@users.noreply.github.com>
2022-05-12 11:19:51 -0700

    finished module 2

---
+++ classwork/JS practifce/class.js
@@ -0,0 +1,14 @@
+const divEl = document.createElement('div');
+document.body.appendChild(divEl);
+
+let count = 0;
+
+divEl.textcontent = 'Count: ' + count;
+
+const buttonEl = document.createelement('button');
+buttonEl.innertext = 'click to increase count!';
+
+
+
+
+
